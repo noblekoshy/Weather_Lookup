9f30d0688121ef3239652c76959339450dbae30e
noblekoshy <koshy009@umn.edu>
2022-08-07 16:36:36 -0500

    geolocation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, request, redirect, render_template, url_for, flash
+from flask import Flask, request, redirect, render_template, url_for, flash, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,10 +7,22 @@ app = Flask(__name__)
 # open weather API key
 api_key = "a7d7eb5bf1ec72f321958ed911cbd1da"
 
-def get_weather(city):
+def get_weather_city(city):
     # reference https://openweathermap.org/current#name
     # note: lookup by city name will be deprecated soon. Look into geocoding API
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={api_key}"
+    # return response, json method to convert json data to python format data
+    response = requests.get(url).json()
+    return response
+
+def get_weather_coord(lat, lon):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={api_key}"
+    # return response, json method to convert json data to python format data
+    response = requests.get(url).json()
+    return response
+
+def reverse_geocoding(lat, lon):
+    url = f"http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={1}&appid={api_key}"
     # return response, json method to convert json data to python format data
     response = requests.get(url).json()
     return response
@@ -22,7 +34,7 @@ def index():
     if request.method == "POST":
         if request.form.get("city"):
             city = request.form.get("city")
-            r = get_weather(city)
+            r = get_weather_city(city)
             # dict of select weather data
             weather = {
                 'city' : r['name'],
@@ -32,6 +44,7 @@ def index():
                 'icon' : r['weather'][0]['icon']
             }
             return render_template('weather.html', weather = weather)
+
         if request.form.get("random_city"):
             r = requests.get("http://127.0.0.1:5001")
             return render_template('index.html', random_city=r.text)
@@ -43,6 +56,30 @@ def about():
 @app.route('/instructions')
 def insturctions():
     return render_template('instructions.html')
+
+@app.route('/geo', methods= ['GET', 'POST'])
+def geo():
+    if request.method == "GET":
+        return render_template('geo.html')
+    if request.method == "POST":
+        geo_data = request.get_json()
+        #return jsonify(data)
+        if geo_data:
+            lat = geo_data['location']['lat']
+            lon = geo_data['location']['lng']
+            r = get_weather_coord(lat, lon)
+            # dict of select weather data
+            weather = {
+                'city' : r['name'],
+                'country' : r['sys']['country'],
+                'temperature' : r['main']['temp'],
+                'description' : r['weather'][0]['description'],
+                'icon' : r['weather'][0]['icon']
+            }
+            print(weather)
+            # TODO not displaying the weather page, due to POST on geo/ ? how does regular weather work? 
+            # TODO try just getting the city name by reverse geocoding, wouldnt work for Rosvile MN vs CA
+            return render_template('weather.html', weather = weather)
     
 # Listener
 if __name__ == "__main__":
